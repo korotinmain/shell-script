f81b9a08ce89bdd17c0d15d506fa34adb2393d61
Denys Korotin <korotinmain@gmail.com>
2018-11-12 00:32:51 +0200

    Added thread tree

---
+++ Ex6/general.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd ~/Desktop/OperationSystems/Ex6
+
+function processesCreator(){
+	a=1
+	if [ $1 -ge 5 ]
+	then
+		return
+	fi
+	a=$(($a+$1))
+	echo "Process - ${a}"
+	processesCreator $a &
+	processesCreator $a &
+	sleep 1
+}
+
+processesCreator 0
